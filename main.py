3906996a476ab397be00d1dd0ef757546ad4c81d
Taijii-Boy <dao-prog@yandex.ru>
2022-05-19 22:30:16 +0300

    Начал писать загрузчик

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ class YouTubeObject:
     def get_info(self):
         print(f'Канал: {self.__yt.author}')
         print(f'Название: {self.__yt.title}')
-        print(f'Длина: {self.convert_time(self.__yt.length)}')
+        print(f'Длительность: {self.convert_duration(self.__yt.length)}')
 
     @classmethod
-    def convert_time(cls, seconds: int):
-
+    def convert_duration(cls, seconds: int) -> str:
         sec = seconds % (24 * 3600)
         hours = sec // 3600
         sec = sec % 3600
         minutes = sec // 60
         sec = sec % 60
-        return f'{hours}:{minutes}:{sec}'
+        return f'{hours:02}:{minutes:02}:{sec:02}'
 
 
-obj = YouTubeObject('https://www.youtube.com/watch?v=0jZNKV5ROBM')
-obj.get_info()
+if __name__ == '__main__':
+
+    obj = YouTubeObject('https://www.youtube.com/watch?v=0jZNKV5ROBM')
+    obj.get_info()
